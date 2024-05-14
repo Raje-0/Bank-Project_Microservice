bf0e9ba66dacc18dfebd54af373bc0c3439cac8b
kanhaiya kumar <97101583+Raje-0@users.noreply.github.com>
2024-05-14 09:53:10 +0530

    adding profile for all 3 diferent ms

--- Accounts/src/main/java/com/raje/AccountsApplication.java
+++ Accounts/src/main/java/com/raje/AccountsApplication.java
@@ -1,9 +1,42 @@
 package com.raje;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.raje.dto.AccountsContactInfoDto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "RajeBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Kanhaiya raje",
+						email = "rajekanhaiya@gmail.com ",
+						url = "https://www.rajekanhaiya.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.rajekanhaiya.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "rajeBank Accounts microservice REST API Documentation",
+				url = "https://www.rajekanhaiya.com/swagger-ui.html"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
